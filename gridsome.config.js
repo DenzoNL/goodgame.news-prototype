db9fd0722d0bdeb44276c7af918bb11a9eaa3dfb
Dennis Bogers <dennis@bogers.xyz>
2020-08-15 23:16:12 +0200

    style: replace default gridsome css with tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'GoodGame.news',
   plugins: [
     /**
      * Add the Tailwind CSS plugin.
