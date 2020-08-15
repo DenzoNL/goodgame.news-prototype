cdffdceff083fd9431d6b41d3f93dd96fcbd0b92
Dennis Bogers <dennis@bogers.xyz>
2020-08-15 22:25:15 +0200

    feat: implement basic GhostPost template so we can link to posts

---
+++ src/templates/GhostPost.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <article class="prose">
+      <header>
+        <h1 v-html="$page.post.title"></h1>
+      </header>
+      <section v-html="$page.post.content"></section>
+    </article>
+  </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: ghostPost (path: $path) {
+    title
+    path
+    date: published_at (format: "D. MMMM YYYY")
+    tags {
+      id
+      title: name
+      path
+    }
+    description: excerpt
+    content: html
+    coverImage: feature_image
+  }
+}
+</page-query>
