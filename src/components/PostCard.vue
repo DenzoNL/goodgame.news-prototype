f7e65518e43ba707f8d2bdfcdd199898998a7c10
Dennis Bogers <dennis@bogers.xyz>
2020-08-16 00:32:00 +0200

    feat: implement basic home page

---
+++ src/components/PostCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="mt-4 pb-4 border-b">
+    <header>
+      <g-link :to="post.path">
+        <g-image
+          class="rounded w-full hover:opacity-75"
+          v-if="post.coverImage"
+          :src="post.coverImage"
+        ></g-image>
+      </g-link>
+      <g-link :to="post.path">
+        <h2
+          class="text-xl font-semibold hover:underline text-gray-900 mt-2"
+          v-html="post.title"
+        ></h2>
+      </g-link>
+    </header>
+    <section class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+      <p v-html="post.description"></p>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['post'],
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,40 +1,41 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
-        >Gridsome Docs</a
-      >
-      <a
-        href="https://github.com/gridsome/gridsome"
-        target="_blank"
-        rel="noopener"
-        >GitHub</a
-      >
-    </p>
+    <PostCard
+      v-for="edge in $page.posts.edges"
+      :key="edge.node.id"
+      :post="edge.node"
+    >
+    </PostCard>
   </Layout>
 </template>
 
+<page-query>
+{
+  posts: allGhostPost(
+      sortBy: "published_at",
+      order: DESC,
+  ) {
+    edges {
+      node {
+        title
+        description: excerpt
+        date: published_at (format: "D. MMMM YYYY")
+        path
+        slug
+        id
+        coverImage: feature_image
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import PostCard from '../components/PostCard.vue';
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!',
+  components: {
+    PostCard,
   },
 };
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
