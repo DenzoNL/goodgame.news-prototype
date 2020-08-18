5c2fa405db1164a2b7964c2be5c266a9d0cf7080
Dennis Bogers <dennis@bogers.xyz>
2020-08-18 03:24:37 +0200

    fix: navbar flex

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,16 +4,18 @@
       <nav class="flex items-center flex-wrap justify-between py-4">
         <div class="flex items-center justify-start flex-shrink-0">
           <g-link to="/">
-            <g-image src="/logo.svg" class="h-8 mr-16 md:mr-32"></g-image>
+            <g-image src="/logo.svg" class="h-8"></g-image>
           </g-link>
         </div>
         <div
-          class="flex flex-grow justify-between items-center tracking-wider text-sm text-gray-700 px-4 uppercase"
+          class="flex flex-grow justify-end items-center tracking-wider text-sm text-gray-700 px-4 uppercase"
         >
-          <g-link to="/" class="hover:text-indigo-500">News</g-link>
-          <span class="text-indigo-500 text-2xl">&rsaquo;</span>
-          <g-link to="/reviews" class="hover:text-indigo-500">Reviews</g-link>
-          <span class="text-indigo-500 text-2xl">&rsaquo;</span>
+          <g-link to="/" class="hover:text-indigo-500 mr-2">News</g-link>
+          <span class="text-indigo-500 text-2xl mr-2">&rsaquo;</span>
+          <g-link to="/reviews" class="hover:text-indigo-500 mr-2"
+            >Reviews</g-link
+          >
+          <span class="text-indigo-500 text-2xl mr-2">&rsaquo;</span>
           <g-link to="/about" class="hover:text-indigo-500">About</g-link>
         </div>
       </nav>
