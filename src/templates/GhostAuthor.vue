09f1d9ed65ac89e7a43e5aae8b21d0b177f702f9
Dennis Bogers <dennis@bogers.xyz>
2020-08-18 06:13:17 +0200

    feat: add GhostAuthor template

---
+++ src/templates/GhostAuthor.vue
@@ -0,0 +1,68 @@
+<template>
+  <Layout>
+    <div class="container max-w-2xl mx-auto mt-4 px-4">
+      <div class="border-b pb-4">
+        <div class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+          <h1>
+            <span class="text-gray-700">All posts by</span> @{{
+              $page.author.name
+            }}
+          </h1>
+        </div>
+      </div>
+      <PostCard
+        v-for="edge in $page.author.belongsTo.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      >
+      </PostCard>
+      <!-- <Pager :pageInfo="$page.posts.pageInfo"></Pager> -->
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Author ($path: String!) {
+  author:ghostAuthor (path: $path) {
+    name
+    path
+    profile_image
+    belongsTo {
+      edges {
+        node {
+          ...on GhostPost {
+            title
+            description: excerpt
+            date: published_at
+            path
+            reading_time
+            tags {
+              id
+              title: name
+              path
+            }
+            authors {
+              id
+              name
+              slug
+            }
+            slug
+            id
+            coverImage: feature_image
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import PostCard from '../components/PostCard.vue';
+
+export default {
+  components: {
+    PostCard,
+  },
+};
+</script>
