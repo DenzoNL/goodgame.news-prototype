d85de097a8ab9472e18d36a0a231f18fa6197c47
Dennis Bogers <dennis@bogers.xyz>
2020-08-23 16:30:45 +0200

    feat: implement GhostPage template

---
+++ src/templates/GhostPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <div class="container max-w-2xl mx-auto flex-grow">
+      <div class="mt-4 px-4">
+        <article class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+          <header>
+            <h1 v-html="$page.page.title"></h1>
+          </header>
+          <section v-html="$page.page.content"></section>
+        </article>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Page($path: String!) {
+  page: ghostPage (path: $path) {
+    title
+    content: html
+  }
+}
+</page-query>
